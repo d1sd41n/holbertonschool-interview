b422ea8f451f9048f47317c918e579baf1c50f35
Daniel Pérez <1498@holbertonschool.com>
2021-01-12 11:00:09 -0500

    init

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void sum_grids(int grid1[3][3], int grid2[3][3]);
+int is_sandpile(int grid1[3][3]);
+void convert_sandpile(int grid1[3][3]);
+
+#endif
