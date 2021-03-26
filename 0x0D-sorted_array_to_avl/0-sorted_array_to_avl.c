7a125c48da4d85737703c909d789bbf7ecc9d6ab
Daniel Perez <1498@holbertonschool.com>
2021-03-25 22:00:11 -0500

    0

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,85 +1,80 @@
 #include "binary_trees.h"
 
+
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: pointer to the parent node of the node to create
- * @value: value to put in the new node
- * Return: a pointer to the new node, or NULL on failure
+* sorted_array_to_avl - doc
+* @array: doc
+* @size: doc
+* Return: doc
+*/
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+avl_t *tree = NULL;
+
+if (array == NULL || size == 0)
+return (NULL);
+
+add_middle_value(&tree, array, 0, (int) size - 1);
+
+return (tree);
+}
+
+
+/**
+* add_middle_value - doc
+*
+* @tree: - doc
+* @array: doc
+* @idx_l: doc.
+* @idx_r: doc
+* Return: doc
+*/
+avl_t *add_middle_value(avl_t **tree, int *array, int idx_l, int idx_r)
+{
+int value = 0;
+size_t value_idx = 0;
+avl_t *node = NULL;
+
+if (tree == NULL || idx_r < idx_l)
+return (NULL);
+
+value_idx = (idx_r  - idx_l) / 2 + idx_l;
+value = array[value_idx];
+
+node = binary_tree_node(NULL, value);
+if (node == NULL)
+return (NULL);
+if (*tree == NULL)
+*tree = node;
+
+node->left = add_middle_value(tree, array, idx_l, value_idx - 1);
+if (node->left != NULL)
+(node->left)->parent = node;
+node->right = add_middle_value(tree, array, value_idx + 1, idx_r);
+if (node->right != NULL)
+(node->right)->parent = node;
+
+return (node);
+}
+
+
+/**
+* binary_tree_node - doc
+* @parent: doc
+* @value: doc
+* Return: doc
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = NULL;
+binary_tree_t *node = NULL;
 
-	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL)
-		return (NULL);
-	node->n = value;
-	node->parent = parent;
-	node->left = NULL;
-	node->right = NULL;
+node = malloc(sizeof(binary_tree_t));
+if (node == NULL)
+return (NULL);
+node->n = value;
+node->parent = parent;
+node->left = NULL;
+node->right = NULL;
 
-	return (node);
-}
-
-/**
- * add_middle_value - create node with value in the middle of the array.
- *
- * @tree: - double pointer to root of new tree.
- * @array: array with the values of nodes.
- * @idx_l: left index of subarray.
- * @idx_r: right index of subarray.
- *
- * Return: new node created.
- */
-
-avl_t *add_middle_value(avl_t **tree, int *array, int idx_l, int idx_r)
-{
-	int value = 0;
-	size_t value_idx = 0;
-	avl_t *node = NULL;
-
-	if (tree == NULL || idx_r < idx_l)
-		return (NULL);
-
-	value_idx = (idx_r  - idx_l) / 2 + idx_l;
-	value = array[value_idx];
-
-	node = binary_tree_node(NULL, value);
-	if (node == NULL)
-		return (NULL);
-	if (*tree == NULL)
-		*tree = node;
-
-	node->left = add_middle_value(tree, array, idx_l, value_idx - 1);
-	if (node->left != NULL)
-		(node->left)->parent = node;
-	node->right = add_middle_value(tree, array, value_idx + 1, idx_r);
-	if (node->right != NULL)
-		(node->right)->parent = node;
-
-	return (node);
-}
-
-
-
-/**
- * sorted_array_to_avl - function that builds an AVL tree from an array
- *
- * @array: is a pointer to the first element of the array to be converted
- * @size: number of element in the array
- *
- * Return: a pointer to the root node of the created AVL tree,
- * or NULL on failure
- * You can assume there will be no duplicate value in the array
- */
-avl_t *sorted_array_to_avl(int *array, size_t size)
-{
-	avl_t *tree = NULL;
-
-	if (array == NULL || size == 0)
-		return (NULL);
-
-	add_middle_value(&tree, array, 0, (int) size - 1);
-
-	return (tree);
+return (node);
 }
--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -1,10 +1,8 @@
 
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -20,12 +18,9 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 avl_t *sorted_array_to_avl(int *array, size_t size);
-
 #endif /*HOLBERTON_H*/
