d6f21b9d7d19cc96261ff7b7d76eaac645eee908
Daniel Perez <1498@holbertonschool.com>
2021-05-27 19:51:19 -0500

    0

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+
+#!/usr/bin/python3
+"""[summary]
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """[summary]
+
+    Args:
+        matrix ([type]): [description]
+    """
+    matrix.reverse()
+    m = [i.copy()
+         for i in matrix]
+    for i, row in enumerate(m):
+        for j, col in enumerate(row):
+            matrix[j][i] = m[i][j]
