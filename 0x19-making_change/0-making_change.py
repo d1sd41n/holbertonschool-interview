7596224ce266bd1aa16021aaf27b498f5e33ebf5
Daniel Pérez <1498@holbertonschool.com>
2021-06-24 21:57:07 -0500

    0

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -15,17 +15,21 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
+    if not coins:
+        return -1
     coins.sort(reverse=True)
-    index = 0
-    aux = 0
-    tota_sum = 0
-    while (tota_sum < total
-           and index < len(coins)):
-        while coins[index] <= total \
-                - tota_sum:
-            tota_sum += coins[index]
-            aux += 1
-            if tota_sum == total:
-                return aux
-        index += 1
+    if total < coins[-1]:
+        return -1
+    dx_num = 0
+    num_coins = 0
+    while (total > 0 and
+           dx_num < len(coins)):
+        coin_val = coins[dx_num]
+        factor = total // coin_val
+        if factor != 0:
+            num_coins += factor
+            total -= factor * coin_val
+        dx_num += 1
+    if total == 0:
+        return num_coins
     return -1
