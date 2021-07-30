83ba82c337ec7911d6caa355db393d1d5eb211f5
Daniel Pérez <1498@holbertonschool.com>
2021-07-29 23:22:16 -0500

    0

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
-* _strlen - DOC
-* @s: DOC
-* Return: DOC
+* _strlen - Fdc
+* @s: sdc
+* Return: dc
 **/
 
 int _strlen(char *s)
@@ -17,22 +17,21 @@ return (_strlen(s) + 1);
 }
 
 /**
-* _cmp - V
-* @s1: fV
-* @s2: sDOC
-* @i: DOC
-* @j: DOC
-* @wc: DOC
-* @lwc: DOC
-* @len: lDOC
-* Return: DOC
+* _cmp - dc
+* @s1: v
+* @s2: dc
+* @i: dc
+* @j: dc
+* @wc: dc
+* @lwc: ldc
+* @len: ldc
+* Return: 1dc
 **/
 
 int _cmp(char *s1, char *s2, int i, int j, int wc, int lwc, int len)
 {
 if (s2[j] == '\0' && (s2[j - 1] == '*' || s2[j - 1] == s1[len - 1]))
 return (1);
-
 if (s2[j] == '*')
 {
 lwc = ++j;
@@ -44,14 +43,12 @@ if (s2[j] == s1[i])
 i++;
 j++;
 wc = 0;
-
 return (_cmp(s1, s2, i, j, wc, lwc, len));
 }
 if (s1[i] != s2[j] && wc == 1)
 {
 if (s1[i] == '\0')
 return (0);
-
 i++;
 return (_cmp(s1, s2, i, j, wc, lwc, len));
 }
@@ -63,17 +60,16 @@ j = lwc;
 wc = 1;
 if (lwc == 0)
 return (0);
-
 return (_cmp(s1, s2, i, j, wc, lwc, len));
 }
 return (0);
 }
 
 /**
-* wildcmp - DOC
-* @s1: DOC
-* @s2: DOC
-* Return: DOC
+* wildcmp - dc
+* @s1: dc
+* @s2: dc
+* Return: dc
 */
 
 int wildcmp(char *s1, char *s2)
@@ -83,3 +79,4 @@ int i = 0, j = 0, wc = 1, lwc = 0, len;
 len = _strlen(s1);
 
 return (_cmp(s1, s2, i, j, wc, lwc, len));
+}
